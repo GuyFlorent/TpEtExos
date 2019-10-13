5279273a6c33901ebfd543975c81c2d30fd78209
WANGUEU <wangueujunior23@gmail.com>
2019-10-13 08:53:53 +0200

    Ajout de test dans le Main

---
+++ TpYoutube/TpYoutube/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace TpYoutube
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Data Source=localhost;Initial Catalog=devoir;Uid=root;Pwd=;";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection();
+
+
+                conn.ConnectionString = connectionString;
+            tes
+            conn.Open();
+        }
+    }
+}
